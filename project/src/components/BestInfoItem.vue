afc843a931d50cc8a3b12e828e8568aac5e36185
miquelven <miquelven.silva@gmail.com>
2024-01-16 09:06:02 -0300

    adição scoreview e bestview

---
+++ project/src/components/BestInfoItem.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  props: ["score"],
+};
+</script>
+
+<template>
+  <td class="text-4xl">
+    {{ score }}
+  </td>
+</template>
--- project/src/views/BestView.vue
+++ project/src/views/BestView.vue
@@ -1,11 +1,21 @@
 <script>
 import Header from "./icons/Header.vue";
 import Footer from "./icons/Footer.vue";
+import BestInfoItem from "@/components/BestInfoITem.vue";
 
 export default {
   components: {
     Header,
     Footer,
+    BestInfoItem,
+  },
+  data() {
+    return {
+      scores: ["00", "00", "00", "00", "00", "00", "00", "00", "00", "00"],
+    };
+  },
+  mounted() {
+    // faz a request do historico e armazena na variavel para fazer o 'for'
   },
 };
 </script>
@@ -14,8 +24,25 @@ export default {
   <Header />
   <main class="w-full my-19" id="best">
     <!-- container -->
-    <div class="max-w-screen-2xl m-auto flex justify-center">
-      Create a table
+    <div class="max-w-screen-2xl m-auto">
+      <table class="grid grid-cols-1 gap-7">
+        <tr>
+          <th>
+            <h1
+              class="text-4xl bg-clip-text bg-gradient-to-r from-yellow-400 from-10% via-yellow-100 via-30% to-yellow-400 to-90% text-transparent mb-10"
+            >
+              Suas melhores pontuações:
+            </h1>
+          </th>
+        </tr>
+        <tr
+          v-for="(score, index) in scores"
+          :key="index"
+          class="even:text-white"
+        >
+          <BestInfoItem :score="score" />
+        </tr>
+      </table>
     </div>
   </main>
   <Footer />
