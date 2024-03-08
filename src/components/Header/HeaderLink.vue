aa9619fb6561ff5e12e3c6e88ae33ac16d912028
miquelven <miquelven.silva@gmail.com>
2024-03-08 15:29:28 -0300

    pequena mudança

---
+++ src/components/Header/HeaderLink.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  props: ["text", "to"],
+};
+</script>
+
+<template>
+  <router-link :to="to"
+    ><span
+      class="transition duration-300 ease-in py-8 text-zinc-400 hover:text-zinc-200 relative"
+    >
+      {{ text }}
+    </span>
+  </router-link>
+</template>
