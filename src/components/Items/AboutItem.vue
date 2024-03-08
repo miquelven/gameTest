eda6a55a029d6e721dabdec7c840b7f33c5367f3
miquelven <miquelven.silva@gmail.com>
2024-03-08 16:50:31 -0300

    mudança na cor

--- src/components/Items/AboutItem.vue
+++ src/components/Items/AboutItem.vue
@@ -13,11 +13,11 @@ export default {
   <div :class="formatStyllingClass">
     <h3
       id="title"
-      class="text-5xl font-bold relative inline mb-4 max-md:text-4xl max-sm:text-3xl before:content-[''] before:absolute before:bg-gradient-to-r before:from-[rgb(201,201,10)] before:to-[#000000ca] before:-bottom-[6px] before:h-1 before:w-full"
+      class="text-5xl font-medium relative inline mb-4 max-md:text-4xl max-sm:text-3xl before:content-[''] before:absolute before:bg-gradient-to-r before:from-[#69dcaa] before:to-[#0a0a0a] before:-bottom-[6px] before:h-1 before:w-full"
     >
       {{ title }}
     </h3>
-    <p class="text-xl text-gray-200/90 max-md:text-base max-[440px]:text-sm">
+    <p class="text-xl text-zinc-300 max-md:text-base max-[440px]:text-sm">
       {{ content }}
     </p>
   </div>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -24,7 +24,7 @@ export default {
 <template>
   <Header />
   <div
-    class="m-auto mb-10 mt-28 max-w-4xl border-x-2 border-white/50 px-5 py-10 flex flex-col gap-24 max-sm:border-0"
+    class="m-auto mb-10 mt-28 max-w-4xl border-x-2 border-[#2bb478] px-5 py-10 flex flex-col gap-24 max-sm:border-0"
   >
     <AboutItem
       v-for="({ title, content }, index) in itemInfo"
