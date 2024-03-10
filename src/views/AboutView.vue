a4b36ee61941dfa079e5f346164c1e060553baa7
miquelven <miquelven.silva@gmail.com>
2024-03-09 23:03:55 -0300

    mudança nos espaçamentos e remoção da borda

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -23,9 +23,7 @@ export default {
 
 <template>
   <Header />
-  <div
-    class="m-auto mb-10 mt-28 max-w-4xl border-x-2 border-[#2bb478] px-5 py-10 flex flex-col gap-24 max-sm:border-0"
-  >
+  <div class="m-auto mb-5 mt-14 max-w-5xl px-5 py-10 flex flex-col gap-24">
     <AboutItem
       v-for="({ title, content }, index) in itemInfo"
       :key="index"
