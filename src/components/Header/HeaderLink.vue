a27bc45f899e018beecc2ee6182378457d90a2df
miquelven <miquelven.silva@gmail.com>
2024-03-09 23:19:27 -0300

    mudança no peso da fonte

--- src/components/Header/HeaderLink.vue
+++ src/components/Header/HeaderLink.vue
@@ -7,7 +7,7 @@ export default {
 <template>
   <router-link :to="to"
     ><span
-      class="transition duration-300 ease-in py-8 relative hover:text-white hover:font-bold before:content-[''] before:absolute before:right-0 before:bottom-[20px] before:w-0 before:overflow-hidden before:border-b-4 before:border-transparent before:transition-all before:duration-300 hover:before:w-6/12 hover:before:border-[#2bb478]"
+      class="transition duration-300 ease-in py-8 relative hover:text-white hover:font-medium before:content-[''] before:absolute before:right-0 before:bottom-[20px] before:w-0 before:overflow-hidden before:border-b-4 before:border-transparent before:transition-all before:duration-300 hover:before:w-6/12 hover:before:border-[#2bb478]"
     >
       {{ text }}
     </span>
