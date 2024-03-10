d0f3aefb60fa65872f91f549fb149ffd1f3b94ab
miquelven <miquelven.silva@gmail.com>
2024-03-09 23:03:35 -0300

    mudança no texto

--- src/components/Items/AboutItem.vue
+++ src/components/Items/AboutItem.vue
@@ -13,11 +13,11 @@ export default {
   <div :class="formatStyllingClass">
     <h3
       id="title"
-      class="text-5xl font-medium relative inline mb-4 max-md:text-4xl max-sm:text-3xl before:content-[''] before:absolute before:bg-gradient-to-r before:from-[#69dcaa] before:to-[#0a0a0a] before:-bottom-[6px] before:h-1 before:w-full"
+      class="text-4xl font-medium relative inline mb-7 max-md:text-3xl max-sm:text-2xl before:content-[''] before:absolute before:bg-gradient-to-r before:from-[#69dcaa] before:to-[#0a0a0a] before:-bottom-[6px] before:h-1 before:w-full"
     >
       {{ title }}
     </h3>
-    <p class="text-xl text-zinc-300 max-md:text-base max-[440px]:text-sm">
+    <p class="text-lg font-light text-zinc-300 leading-7 max-[440px]:text-sm">
       {{ content }}
     </p>
   </div>
