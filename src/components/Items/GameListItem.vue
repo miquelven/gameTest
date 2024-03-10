c654b1378224e7444c4003c6bcd52bed02eee5ac
miquelven <miquelven.silva@gmail.com>
2024-03-09 22:49:54 -0300

    adição do componente v-hover

--- src/components/Items/GameListItem.vue
+++ src/components/Items/GameListItem.vue
@@ -1,45 +1,29 @@
 <script>
-import { useElementHover } from "@vueuse/core";
-
 export default {
   props: ["item"],
-  data() {
-    return {
-      isHovered: false,
-    };
-  },
-  mounted() {
-    this.isHovered = useElementHover(this.$refs.myHoverableElement);
-  },
-  computed: {
-    isHoveredCard() {
-      return this.isHovered ? "tonal" : "outlined";
-    },
-    isHoveredCardColor() {
-      return !this.isHovered ? "#777" : "#2bb478";
-    },
-  },
 };
 </script>
 
 <template>
-  <v-card
-    :key="index"
-    :color="isHoveredCardColor"
-    :variant="isHoveredCard"
-    class="mx-auto my-8"
-    elevation="16"
-    max-width="344"
-    min-height="357"
-    ref="myHoverableElement"
-  >
-    <v-img height="200px" :src="item.img" cover></v-img>
-    <v-card-item>
-      <v-card-title> {{ item.name }} </v-card-title>
-    </v-card-item>
+  <v-hover v-slot="{ isHovering, props }">
+    <v-card
+      v-bind="props"
+      :color="isHovering ? '#2bb478' : '#777'"
+      :variant="isHovering ? 'tonal' : 'outlined'"
+      class="mx-auto my-8 py-5"
+      elevation="16"
+      max-width="344"
+      height="400"
+      rounded="lg"
+    >
+      <v-img height="200px" :src="item.img" cover></v-img>
+      <v-card-item>
+        <v-card-title> {{ item.name }} </v-card-title>
+      </v-card-item>
 
-    <v-card-text>
-      {{ item.tip }}
-    </v-card-text>
-  </v-card>
+      <v-card-text>
+        {{ item.tip }}
+      </v-card-text>
+    </v-card>
+  </v-hover>
 </template>
