3cba435cce71220011ca86ccce2bb7ead84ba7d0
miquelven <miquelven.silva@gmail.com>
2024-03-11 16:15:04 -0300

    mudança no layout

--- src/views/GamesPage.vue
+++ src/views/GamesPage.vue
@@ -8,6 +8,7 @@ import Simon from "@/components/Games/Simon.vue";
 import Snake from "@/components/Games/Snake.vue";
 import SpaceInvaders from "@/components/Games/SpaceInvaders.vue";
 import axios from "axios";
+import Button from "@/views/icons/Button.vue";
 
 export default {
   components: {
@@ -19,12 +20,13 @@ export default {
     Simon,
     Snake,
     SpaceInvaders,
+    Button,
   },
   data() {
     return {
       showModal: true,
       score: [],
-      counter: 0,
+      counter: 3,
       intervalCounter: null,
       minutes: 0,
       seconds: 0,
@@ -99,7 +101,7 @@ export default {
   >
     <router-link
       to="/"
-      class="transition-all absolute top-20 left-20 px-3 py-1 shadow-md shadow-yellow-300 text-2xl hover:text-3xl hover:shadow-lg hover:shadow-white/30"
+      class="transition-all duration-300 rounded-md absolute top-20 left-20 px-3 py-1 text-2xl hover:text-3xl"
     >
       <font-awesome-icon :icon="['fas', 'arrow-left']" />
     </router-link>
@@ -122,25 +124,20 @@ export default {
         }"
       >
         <div
-          class="bg-white text-black flex flex-col gap-8 p-5 h-96 w-96 absolute rounded-xl border-8 border-gray-800 font-bold"
+          class="flex flex-col gap-9 p-5 h-96 w-96 absolute rounded-lg border-2 border-[#777] font-light"
         >
           <h2>Há um contador em todos os jogos</h2>
           <p>Termine todos no menor tempo possível</p>
           <p>Poderá ver sua pontuação assim que terminar os jogos.</p>
           <p>O contador iniciará assim que fechar essa janela</p>
-          <button
-            @click="startCounter"
-            class="bg-black/50 rounded-md text-white py-2 hover:bg-black/70"
-          >
-            Continuar
-          </button>
+          <Button :click="startCounter" type="button" label="Continuar" />
         </div>
       </div>
     </template>
 
     <!-- temporizador -->
     <input
-      class="text-4xl bg-transparent text-center font-bold absolute top-20"
+      class="text-4xl bg-transparent text-center font-bold absolute top-20 max-sm:text-2xl"
       type="text"
       placeholder="00:00"
       v-model="timerController"
@@ -148,7 +145,8 @@ export default {
     />
     <!-- area dos jogos -->
     <div
-      class="absolute rounded-xl flex justify-center items-center border-8 border-double border-yellow-500 shadow-lg shadow-orange-200/30 size-2/3 max-h-[67%]"
+      class="absolute rounded-lg flex justify-center items-center border-4 border-double border-[#777] shadow-sm shadow-[#2bb478] size-2/3 max-h-[67%]"
+      v-if="counter <= 10"
     >
       <template v-if="counter == 0 && !showModal">
         <TicTacToe @addCounter="addCounter" @addScore="addScore" />
@@ -181,32 +179,29 @@ export default {
       <template v-else-if="counter == 7">
         <SpaceInvaders @addCounter="addCounter" @addScore="addScore" />
       </template>
-
-      <!-- RESULTADO -->
-      <template v-if="counter >= 8">
-        <div
-          class="bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-yellow-500 via-red-600 to-fuchsia-500 shadow-[5px_7px_10px_#666] flex flex-col gap-8 pt-14 px-5 h-96 w-96 absolute rounded-xl font-bold"
-        >
-          <h2 class="text-4xl text-center mb-4">Fim dos jogos</h2>
-          <div class="flex justify-around">
-            <p class="text-lg text-black/70">Seu tempo:</p>
-            <span class="text-white font-bold text-2xl">{{
-              timerController
-            }}</span>
-          </div>
-          <div class="flex justify-around">
-            <p class="text-lg text-black/70">Sua pontuação:</p>
-            <span class="text-white font-bold text-2xl">{{ formatScore }}</span>
-          </div>
-          <router-link
-            to="/"
-            @click="startCounter"
-            class="bg-black/50 rounded-md text-white py-2 hover:bg-black/70 mt-10 text-center"
-          >
-            Voltar
-          </router-link>
-        </div>
-      </template>
     </div>
+    <!-- RESULTADO -->
+    <template v-if="counter >= 8">
+      <div
+        class="border-2 border-[#2bb478] flex flex-col gap-8 pt-10 px-24 h-96 absolute rounded-xl font-medium max-sm:px-16"
+      >
+        <h2 class="text-4xl text-center mb-4 max-sm:text-3xl">Fim dos jogos</h2>
+        <div class="flex justify-around">
+          <p class="text-lg max-sm:text-base">Seu tempo:</p>
+          <span class="text-white text-2xl max-sm:text-xl">{{
+            timerController
+          }}</span>
+        </div>
+        <div class="flex justify-around">
+          <p class="text-lg max-sm:text-base">Sua pontuação:</p>
+          <span class="text-white text-2xl max-sm:text-xl">{{
+            formatScore
+          }}</span>
+        </div>
+        <router-link to="/" @click="startCounter" class="self-end mt-10">
+          <Button label="Voltar" type="button" />
+        </router-link>
+      </div>
+    </template>
   </div>
 </template>
