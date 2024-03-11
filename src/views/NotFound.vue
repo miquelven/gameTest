74621df0c4621a96fb1814e38fe033e3db844ceb
miquelven <miquelven.silva@gmail.com>
2024-03-11 14:50:14 -0300

    mudança na estilização

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,21 +1,26 @@
+<script>
+import Button from "@/views/icons/Button.vue";
+
+export default {
+  components: { Button },
+};
+</script>
+
 <template>
   <div
     class="w-full h-[calc(100vh-80px)] m-auto flex flex-col justify-center items-center max-sm:scale-75"
   >
     <div
-      class="bg-gradient-to-r from-red-800 via-yellow-600 to-yellow-500 py-8 px-16 rounded-xl shadow-[55px_35px_40px_rgba(75,75,75,.5)] text-lg"
+      class="bg-gradient-to-tl from-transparent from-20% via-transparent via-40% to-[#2bb478] py-8 px-16 rounded-lg text-lg"
     >
       <div>
-        <h1 class="font-bold text-7xl">Erro: 404</h1>
-        <p class="my-2">Página Não Encontrada!</p>
+        <h4 class="font-medium text-6xl">Erro: 404</h4>
+        <p class="my-2 font-light">Página Não Encontrada!</p>
       </div>
-      <div class="mt-10 flex flex-col">
-        <p>Ir para a página principal?</p>
-        <router-link
-          to="/"
-          class="text-center transition-all m-auto mt-10 border-2 w-full rounded-md border-white/50 py-1 hover:bg-[rgba(0,0,0,.3)] hover:border-transparent hover:font-bold"
-        >
-          Página Principal
+      <div class="mt-10 flex flex-col justify-center items-center gap-5">
+        <p class="font-light">Ir para a página principal?</p>
+        <router-link to="/">
+          <Button type="button" label="Página Principal" />
         </router-link>
       </div>
     </div>
