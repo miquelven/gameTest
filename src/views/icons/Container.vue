ca2181522c4e0cda6eceba546de3b0309bee3963
miquelven <miquelven.silva@gmail.com>
2024-03-12 21:24:10 -0300

    adição do padding

--- src/views/icons/Container.vue
+++ src/views/icons/Container.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative max-w-screen-2xl min-h-screen max-2xl:max-w-screen-xl max-xl:max-w-screen-lg m-auto max-lg:max-w-screen-md max-md:max-w-96"
+    class="relative max-w-screen-2xl px-10 min-h-screen max-2xl:max-w-screen-xl max-xl:max-w-screen-lg m-auto max-lg:max-w-screen-md max-md:max-w-96"
   >
     <slot />
   </div>
