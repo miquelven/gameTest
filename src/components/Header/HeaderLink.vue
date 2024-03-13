66b3f384b3b89c0360ff1a066a114d23f70342a7
miquelven <miquelven.silva@gmail.com>
2024-03-12 21:24:23 -0300

    mudança na estilização

--- src/components/Header/HeaderLink.vue
+++ src/components/Header/HeaderLink.vue
@@ -6,9 +6,7 @@ export default {
 
 <template>
   <router-link :to="to"
-    ><span
-      class="transition duration-300 ease-in py-8 relative hover:text-white hover:font-medium before:content-[''] before:absolute before:right-0 before:bottom-[20px] before:w-0 before:overflow-hidden before:border-b-4 before:border-transparent before:transition-all before:duration-300 hover:before:w-6/12 hover:before:border-[#2bb478]"
-    >
+    ><span class="">
       {{ text }}
     </span>
   </router-link>
--- src/components/Header/HeaderMenuMobile/LinksMenu.vue
+++ src/components/Header/HeaderMenuMobile/LinksMenu.vue
@@ -17,7 +17,11 @@ export default {
     v-for="(link, linkIndex) in dataLinks.links"
     :key="linkIndex"
     :to="link"
-    class="w-full text-center"
+    :class="
+      isMobile
+        ? `w-full text-center`
+        : `transition duration-300 ease-in py-8 relative hover:font-medium before:content-[''] before:absolute before:right-0 before:bottom-[20px] before:w-0 before:overflow-hidden before:border-b-4 before:border-transparent before:transition-all before:duration-300 hover:before:w-6/12 hover:before:border-[#2bb478]`
+    "
     ><span class="py-8 font-light">
       {{ dataLinks.texts[linkIndex] }}
     </span>
