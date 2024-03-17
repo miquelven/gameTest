c6cb1ef99231138a9712f1cb56f06ba188a60582
miquelven <miquelven.silva@gmail.com>
2024-03-16 23:44:26 -0300

    remoção do container e da shadow no mobile

--- src/views/ScoreView.vue
+++ src/views/ScoreView.vue
@@ -1,12 +1,10 @@
 <script>
 import TopInfoItem from "@/components/Items/TopInfoItem.vue";
-import Container from "@/views/icons/Container.vue";
 import axios from "axios";
 
 export default {
   components: {
     TopInfoItem,
-    Container,
   },
   data() {
     return {
@@ -33,11 +31,39 @@ export default {
 </script>
 
 <template>
-  <Container>
-    <section class="w-full min-h-[calc(100vh-80px)]">
-      <div class="min-w-screen-2xl m-auto flex justify-center">
-        <table
-          class="grid grid-cols-1 gap-10 p-20 bg-[rgba(0,0,0,.9)] shadow-sm shadow-zinc-700 rounded-xl max-sm:p-10"
+  <section class="w-full min-h-[calc(100vh-80px)] mt-44">
+    <div class="min-w-screen-2xl m-auto flex justify-center">
+      <table
+        class="grid grid-cols-1 gap-10 p-20 bg-[rgba(0,0,0,.9)] shadow-sm shadow-zinc-700 rounded-xl max-sm:p-10 max-sm:shadow-none"
+        v-motion
+        :initial="{
+          opacity: 0,
+        }"
+        :enter="{
+          opacity: 1,
+          transition: {
+            duration: 700,
+            type: 'keyframes',
+            ease: 'easein,',
+          },
+        }"
+      >
+        <tr class="flex justify-center">
+          <th>
+            <h3
+              class="text-4xl font-medium bg-clip-text bg-gradient-to-r from-[#40d292] from-10% via-[#69dcaa] via-30% to-[#2bb478] to-90% text-transparent mb-7 max-[550px]:text-3xl"
+            >
+              Top Pontuações:
+            </h3>
+          </th>
+        </tr>
+        <tr
+          v-for="(item, index) in topScores"
+          :key="index"
+          class="flex font-bold justify-between items-center text-xl gap-20 px-7 py-2 rounded-md border-2 max-[550px]:text-lg max-[550px]:gap-12 max-[375px]:text-sm"
+          :style="{
+            borderColor: index < 3 ? '#40d292' : '#f1f1ef',
+          }"
           v-motion
           :initial="{
             opacity: 0,
@@ -47,44 +73,14 @@ export default {
             transition: {
               duration: 700,
               type: 'keyframes',
-              ease: 'easein,',
+              ease: 'easein',
+              delay: 400,
             },
           }"
         >
-          <tr class="flex justify-center">
-            <th>
-              <h3
-                class="text-4xl font-medium bg-clip-text bg-gradient-to-r from-[#40d292] from-10% via-[#69dcaa] via-30% to-[#2bb478] to-90% text-transparent mb-7 max-[550px]:text-3xl"
-              >
-                Top Pontuações:
-              </h3>
-            </th>
-          </tr>
-          <tr
-            v-for="(item, index) in topScores"
-            :key="index"
-            class="flex font-bold justify-between items-center text-xl gap-20 px-7 py-2 rounded-md border-2 max-[550px]:text-lg max-[550px]:gap-12 max-[375px]:text-sm"
-            :style="{
-              borderColor: index < 3 ? '#40d292' : '#f1f1ef',
-            }"
-            v-motion
-            :initial="{
-              opacity: 0,
-            }"
-            :enter="{
-              opacity: 1,
-              transition: {
-                duration: 700,
-                type: 'keyframes',
-                ease: 'easein',
-                delay: 400,
-              },
-            }"
-          >
-            <TopInfoItem :data="item" :position="index" />
-          </tr>
-        </table>
-      </div>
-    </section>
-  </Container>
+          <TopInfoItem :data="item" :position="index" />
+        </tr>
+      </table>
+    </div>
+  </section>
 </template>
