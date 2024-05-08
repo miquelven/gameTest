53f27ba1677cf57d7bf0c6abaa6e69ccc74aa0c4
miquelven <miquelven.silva@gmail.com>
2024-05-08 13:46:48 -0300

    feat: add bannerhome

---
+++ src/components/BannerHome/index.vue
@@ -0,0 +1,51 @@
+<script>
+import Button from "@/views/icons/Button.vue";
+
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
+
+<template>
+  <section
+    class="flex flex-col justify-center gap-40 h-[calc(100vh-212px)] items-center my-28 max-xl:gap-32 max-lg:gap-48 max-sm:my-16 max-sm:h-[calc(100vh-244px)] max-sm:gap-32"
+    id="container"
+  >
+    <div class="text-center">
+      <h1
+        class="text-7xl font-bold max-xl:text-5xl max-sm:text-4xl max-[440px]:text-3xl"
+      >
+        Consiga a <span class="text-[#2bb478]"> melhor pontuação</span> e
+        <span class="block mt-5">Fique em primeiro</span>
+      </h1>
+    </div>
+    <div
+      data-aos="fade-left"
+      data-aos-delay="600"
+      class="flex flex-col justify-center items-center gap-12"
+    >
+      <h2
+        data-aos="fade-right"
+        data-aos-delay="200"
+        class="text-2xl font-semibold text-center leading-9 max-xl:text-xl max-md:text-lg max-sm:text-base max-[440px]:text-sm"
+      >
+        Aqui você encontrará
+        <span class="text-[#2bb478]">vários desafios,</span> terá que superar
+        todos eles no
+        <span class="text-[#2bb478]"> menor tempo possível,</span> dê o seu
+        máximo para conseguir a melhor pontuação.
+      </h2>
+      <router-link
+        to="/gamesPage"
+        data-aos="flip-left"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="500"
+        data-aos-delay="1100"
+      >
+        <Button type="button" label="INICIAR DESAFIO" />
+      </router-link>
+    </div>
+  </section>
+</template>
