bc31f1c2ed43fa5fdfa91760a46b28a4a7c1f034
miquelven <miquelven.silva@gmail.com>
2024-05-08 13:47:18 -0300

    feat: add carousel in mobile device

--- src/components/GameList/index.vue
+++ src/components/GameList/index.vue
@@ -1,9 +1,12 @@
 <script>
+import { Carousel, Navigation, Pagination, Slide } from "vue3-carousel";
 import GameListItem from "../Items/GameListItem.vue";
 import gameListInfo from "@/data/gameListInfo.js";
 
+import "vue3-carousel/dist/carousel.css";
+
 export default {
-  components: { GameListItem },
+  components: { GameListItem, Carousel, Slide, Pagination, Navigation },
   data() {
     return {
       listInfo: "",
@@ -17,22 +20,30 @@ export default {
 </script>
 
 <template>
-  <section class="my-56 max-[440px]:mt-40 max-[440px]:mb-0">
+  <section class="my-56 max-[440px]:mt-80 max-[440px]:mb-0">
     <div
-      class="flex flex-col justify-center items-center gap-16 max-[440px]:gap-8"
+      class="flex flex-col justify-center items-center gap-8 max-[440px]:gap-8"
     >
       <h3
         data-aos="fade-up"
         data-aos-delay="400"
-        class="text-4xl font-semibold text-center max-md:text-3xl max-[440px]:text-2xl"
+        class="text-4xl font-bold text-center max-md:text-3xl max-[440px]:text-3xl"
       >
         Lista dos jogos disponíveis:
       </h3>
+      <h4
+        data-aos="fade-up"
+        data-aos-delay="400"
+        class="max-w-7xl mx-auto text-[#bababa] text-center text-xl max-md:text-lg max-sm:text-base max-[440px]:text-base"
+      >
+        Este é o espaço onde você pode explorar todos os jogos disponíveis para
+        jogar, acompanhados de algumas dicas úteis sobre cada um deles.
+      </h4>
       <div
-        class="py-24 px-14 shadow-sm shadow-[#2bb478] rounded-3xl min-w-96 max-md:py-14 max-md:px-6 max-sm:shadow-transparent"
+        class="py-24 px-14 rounded-md min-w-96 max-md:py-14 max-md:px-6 max-sm:shadow-transparent"
       >
         <ul
-          class="grid gap-16 grid-cols-4 items-center max-lg:grid-cols-2 max-md:grid-cols-1 max-md:gap-11"
+          class="grid gap-4 grid-cols-4 items-center max-xl:justify-center max-xl:grid-cols-[repeat(3,_300px)] max-lg:grid-cols-[repeat(2,_300px)] max-md:hidden"
         >
           <GameListItem
             v-for="(item, index) in listInfo"
@@ -41,6 +52,17 @@ export default {
             :itemIndex="index"
           />
         </ul>
+
+        <div class="hidden max-md:block">
+          <Carousel>
+            <Slide v-for="(item, index) in listInfo" :key="index">
+              <GameListItem :item="item" :itemIndex="index" />
+            </Slide>
+            <template #addons>
+              <Pagination />
+            </template>
+          </Carousel>
+        </div>
       </div>
     </div>
   </section>
