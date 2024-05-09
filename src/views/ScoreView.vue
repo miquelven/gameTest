55c1e44b956159089c719319c62c669cb3892e66
miquelven <miquelven.silva@gmail.com>
2024-05-09 20:17:57 -0300

    feat: add loading when get the datas

--- src/views/ScoreView.vue
+++ src/views/ScoreView.vue
@@ -9,6 +9,7 @@ export default {
   data() {
     return {
       topScores: [],
+      loading: false,
     };
   },
   mounted() {
@@ -16,12 +17,14 @@ export default {
   },
   methods: {
     async fetchTopScores() {
+      this.loading = true;
       const response = await axios("/api/top-scores");
 
       this.topScores = response.data.topScores.map((item) => ({
         ...item,
         name: this.capitalizeFirstLetter(item.name),
       }));
+      this.loading = false;
     },
     capitalizeFirstLetter(string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
@@ -69,27 +72,55 @@ export default {
             </div>
           </th>
         </tr>
-        <tr
-          v-for="(item, index) in topScores"
-          :key="index"
-          class="transition-all duration-300 w-10/12 mx-auto flex justify-between hover:bg-[#0a0a0a] hover:scale-105 items-center text-xl gap-20 px-7 py-4 rounded-md max-sm:w-full"
-          :class="[index < 3 ? 'bg-[#040404]' : '']"
-          v-motion
-          :initial="{
-            opacity: 0,
-          }"
-          :enter="{
-            opacity: 1,
-            transition: {
-              duration: 700,
-              type: 'keyframes',
-              ease: 'easein',
-              delay: 400,
-            },
-          }"
-        >
-          <TopInfoItem :data="item" :position="index" />
-        </tr>
+        <template v-if="!loading && topScores.length > 0">
+          <tr
+            v-for="(item, index) in topScores"
+            :key="index"
+            class="transition-all duration-300 w-10/12 mx-auto flex justify-between hover:bg-[#0a0a0a] hover:scale-105 items-center text-xl gap-20 px-7 py-4 rounded-md max-sm:w-full"
+            :class="[index < 3 ? 'bg-[#040404]' : '']"
+            v-motion
+            :initial="{
+              opacity: 0,
+            }"
+            :enter="{
+              opacity: 1,
+              transition: {
+                duration: 700,
+                type: 'keyframes',
+                ease: 'easein',
+                delay: 400,
+              },
+            }"
+          >
+            <TopInfoItem :data="item" :position="index" />
+          </tr>
+        </template>
+
+        <template v-else>
+          <div
+            class="flex justify-center items-center scale-150 h-[calc(40vh)]"
+          >
+            <div role="status">
+              <svg
+                aria-hidden="true"
+                class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-[#2bb478]"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span class="sr-only">Loading...</span>
+            </div>
+          </div>
+        </template>
       </table>
     </div>
   </section>
