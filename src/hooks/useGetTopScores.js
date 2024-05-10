7e90cc0b5b8543f14f9d71fff87f62c4ca5882a2
miquelven <miquelven.silva@gmail.com>
2024-05-10 10:29:27 -0300

    feat: create custom hook usegettopscores

---
+++ src/hooks/useGetTopScores.js
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/vue-query";
+
+import axios from "axios";
+
+const getTopScore = async () => {
+  const data = await axios("/api/top-scores");
+
+  const newData = data.data.topScores.map((item) => ({
+    ...item,
+    name: item.name.charAt(0).toUpperCase() + string.slice(1),
+  }));
+
+  return newData;
+};
+
+const useGetTopScore = () => {
+  return useQuery({
+    queryKey: ["topscore-data"],
+    queryFn: () => getTopScore(),
+  });
+};
+
+export default useGetTopScore;
