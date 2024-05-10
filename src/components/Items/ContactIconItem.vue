ad40546b366e804258bab716b92e9cdef1faf26d
miquelven <miquelven.silva@gmail.com>
2024-05-10 09:02:22 -0300

    refactor: remove classes and ids and change the div's animation to another div

--- src/components/Items/ContactIconItem.vue
+++ src/components/Items/ContactIconItem.vue
@@ -1,37 +1,35 @@
 <template>
-  <div
-    id="main"
-    :style="{
-      boxShadow: hover ? `0px 0px 10px ${styling}` : 'none',
-    }"
-    class="transition-all overflow-hidden bg-zinc-300 duration-500 ease-in-out inline-block h-[60px] w-[60px] float-left mx-[15px] rounded-full cursor-pointer text-black shadow-[0_10px_10px_rgba(0,0,0,.1)] sm:hover:w-[200px] hover:bg-transparent hover:text-white max-sm:text-white max-sm:h-[45px] max-sm:w-[150px]"
-    @mouseover="hover = true"
-    @mouseout="hover = false"
-    data-aos="zoom-in"
-    data-aos-delay="600"
-  >
+  <div data-aos="zoom-in" data-aos-delay="600" data-aos-once="true">
     <div
-      id="iconArea"
       :style="{
-        backgroundColor: hover ? styling : '',
-        boxShadow: hover ? `162px -4px 50px ${styling}` : 'none',
+        boxShadow: hover ? `0px 0px 10px ${styling}` : 'none',
       }"
-      class="transition-all duration-[1200ms] ease-in-out inline-block w-[60px] h-[60px] rounded-full leading-[60px] text-center max-sm:w-[45px] max-sm:h-[45px] max-sm:leading-[45px]"
+      class="transition-all overflow-hidden bg-zinc-300 duration-500 ease-in-out inline-block h-[60px] w-[60px] float-left mx-[15px] rounded-full cursor-pointer text-black shadow-[0_10px_10px_rgba(0,0,0,.1)] sm:hover:w-[200px] hover:bg-transparent hover:text-white max-sm:text-white max-sm:h-[45px] max-sm:w-[150px]"
+      @mouseover="hover = true"
+      @mouseout="hover = false"
     >
-      <a href="">
-        <font-awesome-icon
-          id="icon"
-          :icon="icon"
-          class="text-xl leading-[60px] max-sm:leading-[45px] max-sm:text-xl"
-        />
-      </a>
+      <div
+        :style="{
+          backgroundColor: hover ? styling : '',
+          boxShadow: hover ? `162px -4px 50px ${styling}` : 'none',
+          color: hover ? 'white' : '',
+        }"
+        class="transition-all duration-[1200ms] ease-in-out inline-block w-[60px] h-[60px] rounded-full leading-[60px] text-center max-sm:w-[45px] max-sm:h-[45px] max-sm:leading-[45px]"
+      >
+        <a href="">
+          <font-awesome-icon
+            :icon="icon"
+            class="text-xl leading-[60px] max-sm:leading-[45px] max-sm:text-xl"
+          />
+        </a>
+      </div>
+      <span
+        id="name"
+        :style="{ color: styling == '#333' ? '#999' : styling }"
+        class="text-sm font-bold ml-[10px] leading-[10px]"
+        >{{ name }}</span
+      >
     </div>
-    <span
-      id="name"
-      :style="{ color: styling == '#333' ? '#999' : styling }"
-      class="text-sm font-bold ml-[10px] leading-[10px]"
-      >{{ name }}</span
-    >
   </div>
 </template>
 
@@ -45,9 +43,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-#main:hover #icon {
-  color: #fff;
-}
-</style>
