b79e2c102cc435733d907ff9557bc3d600865021
miquelven <miquelven.silva@gmail.com>
2024-05-12 23:16:53 -0300

    style: changing button size on tablet

--- src/components/Items/BestInfoItem.vue
+++ src/components/Items/BestInfoItem.vue
@@ -6,7 +6,7 @@ export default {
 
 <template>
   <td
-    class="bg-[#0a0a0a] shadow-sm shadow-white/10 py-4 transition duration-150 ease-in text-4xl w-96 m-auto flex justify-between gap-16 rounded-md px-7 font-medium hover:bg-[#040404] max-2xl:w-72 max-2xl:px-6 max-2xl:text-3xl max-lg:w-52 max-lg:text-2xl max-sm:text-lg max-md:text-xl max-sm:w-full"
+    class="bg-[#0a0a0a] shadow-sm shadow-white/10 py-4 transition duration-150 ease-in text-4xl w-80 m-auto flex justify-between rounded-md px-7 font-medium hover:bg-[#040404] max-2xl:w-72 max-2xl:px-6 max-2xl:text-3xl max-lg:w-52 max-lg:text-xl max-sm:text-lg max-md:text-xl max-md:w-10/12 max-sm:w-full"
     :style="{ backgroundColor: position < 4 ? '#040404' : '' }"
     v-motion
     :initial="{
@@ -21,7 +21,7 @@ export default {
       },
     }"
   >
-    <span class="font-semibold text-sm:text-base">{{ position }} -</span>
+    <span class="font-semibold max-sm:text-base">{{ position }} -</span>
     <p
       class="font-light text-3xl max-xl:text-2xl max-lg:text-xl max-sm:text-sm"
     >
