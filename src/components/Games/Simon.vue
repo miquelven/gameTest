7c327cf13608e8b05ad16d62720a4cf8ca03d9f4
Miquelven Silva <miquelven.silva@gmail.com>
2026-01-20 15:49:07 -0300

    Fix Simon game alignment issues

--- src/components/Games/Simon.vue
+++ src/components/Games/Simon.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="outer-circle"
-    class="bg-[#385a94] rounded-full h-[400px] w-[400px] absolute border-solid border-2 m-auto shadow-[´8_8_15_5_#888888]"
+    class="bg-[#385a94] rounded-full h-[400px] w-[400px] relative border-solid border-2 m-auto shadow-[8px_8px_15px_5px_#888888]"
   >
     <div
       ref="topLeft"
